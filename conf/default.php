82d6fb490ed4567a950ed12e57accb5cc72744bb
Evan Meier <evan@destwin.com>
2020-11-06 16:15:37 -0500

    Starting all over

--- conf/default.php
+++ conf/default.php
@@ -8,5 +8,5 @@ $conf['discussionPage']   = 'discussion:@ID@';
 $conf['userPage']         = 'user:@USER@:';
 $conf['hideTools']        = 0;
 $conf['showIcon']         = 1;
-$conf['icon']             = 3;
-$conf['favicon']          = 3;
+$conf['icon']             = 6;
+$conf['favicon']          = 6;
